63b8e3ff20f0506ed9d8fec2a5c4e95ba16f8949
Vince Mansel <vincemansel@gmail.com>
2011-11-28 20:26:39 -0800

    Initial Commit

---
+++ FlyPlace2/FlyPlace2AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FlyPlace2AppDelegate.h
+//  FlyPlace2
+//
+//  Created by Vince Mansel on 11/28/11.
+//  Copyright (c) 2011 waveOcean Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlyPlace2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ FlyPlace2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlyPlace2
+//
+//  Created by Vince Mansel on 11/28/11.
+//  Copyright (c) 2011 waveOcean Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FlyPlace2AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FlyPlace2AppDelegate class]));
+    }
+}
